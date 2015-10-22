a3646f34df4c94e298ce7b9f5815b9aa2aca6ddb
NAHID <NAHID@NAHID-PC>
2015-10-22 16:44:47 +0600

    First Commit

---
+++ application/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+
+class Pages extends CI_Controller {
+
+	public function view($page = 'home')
+	{
+            $this->load->helper('url');
+            if(!file_exists('application/views/pages/'.$page.'.php')) {show_404 ();}
+            
+            $data['title']= ucfirst($page);
+            $this->load->view('templates/Header',$data);
+            $this->load->view('pages/'.$page);
+            $this->load->view('templates/Footer');
+	}
+        
+}
---
+++ application/views/pages/Home.php
@@ -0,0 +1,305 @@
+
+    
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="main-slideshow">
+                    <div class="flexslider">
+                        <ul class="slides">
+                            <li>
+                                <img src="images/slide1.jpg" />
+                                <div class="slider-caption">
+                                    <h2><a href="blog-single.html">When a Doctor’s Visit Is a Guilt Trip</a></h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="images/slide2.jpg" />
+                                <div class="slider-caption">
+                                    <h2><a href="blog-single.html">Unlocking the scrolls of Herculaneum</a></h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="images/slide3.jpg" />
+                                <div class="slider-caption">
+                                    <h2><a href="blog-single.html">Corin Sworn wins Max Mara Art Prize</a></h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="images/slide4.jpg" />
+                                <div class="slider-caption">
+                                    <h2><a href="blog-single.html">Unlocking the scrolls of Herculaneum</a></h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="images/slide5.jpg" />
+                                <div class="slider-caption">
+                                    <h2><a href="blog-single.html">Corin Sworn wins Max Mara Art Prize</a></h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                                </div>
+                            </li>
+                        </ul> <!-- /.slides -->
+                    </div> <!-- /.flexslider -->
+                </div> <!-- /.main-slideshow -->
+            </div> <!-- /.col-md-12 -->
+            
+            <div class="col-md-4">
+                <div class="widget-item">
+                    <div class="request-information">
+                        <h4 class="widget-title">Vice Chancellor</h4>
+                        <hr />
+                        <div style="display: inline; float: left;">
+                            <img src="images/vc.jpg" width="180" height="180" /><br />
+                            <i>Prof. Dr. Mijanur Rahman</i>
+                        </div>
+
+                        <div style="clear: both;"></div>
+                        <div>                            
+                                <p style="font-size: 13px;">Professor Dr. Mijanur Rahman has joined as Vice-Chancellor of Jagannath University on March 20, 2013. Prior to joining the new post, he was a Professor of the University of Dhaka in the Marketing Department. He was also the Treasurer of Dhaka University. </p>
+                        </div>
+                        
+                        
+                    </div> <!-- /.request-information -->
+                </div> <!-- /.widget-item -->
+            </div> <!-- /.col-md-4 -->
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            
+            <!-- Here begin Main Content -->
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="widget-item">
+                            <h2 class="welcome-text">Welcome to Jagannath University</h2>
+                            <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, adipisci, quibusdam, ad ab quisquam esse aspernatur exercitationem aliquam at fugit omnis vitae recusandae eveniet.</strong></br></br>Inventore, aliquam sequi nisi velit magnam accusamus reprehenderit nemo necessitatibus doloribus molestiae fugit repellat repudiandae dolor. Incidunt, nulla quidem illo suscipit nihil!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, dolorem, fugiat, commodi totam accusantium illo incidunt quis eius eum iure et fugit voluptas atque ratione nobis sed omnis quod ipsa.</br></br>Vivamus mattis nibh vitae dui egestas posuere. Maecenas a est at enim blandit interdum. Cras eget ipsum ac nunc tristique tincidunt sit amet nec quam. Vivamus sed suscipit enim, et dignissim tellus.</p>
+                        </div> <!-- /.widget-item -->
+                    </div> <!-- /.col-md-12 -->
+                </div> <!-- /.row -->
+
+                <div class="row">
+                    
+                    <!-- Show Latest Blog News -->
+                    <div class="col-md-6">
+                        <div class="widget-main">
+                            <div class="widget-main-title">
+                                <h4 class="widget-title">Latest News</h4>
+                            </div> <!-- /.widget-main-title -->
+                            <div class="widget-inner">
+                                <div class="blog-list-post clearfix">
+                                    <div class="blog-list-thumb">
+                                        <a href="blog-single.html"><img src="images/blog/blog-small-thumb1.jpg" alt=""></a>
+                                    </div>
+                                    <div class="blog-list-details">
+                                        <h5 class="blog-list-title"><a href="blog-single.html">Graduate Open Day at the Ruskin</a></h5>
+                                        <p class="blog-list-meta small-text"><span><a href="index.html#">12 January 2014</a></span> with <span><a href="index.html#">3 comments</a></span></p>
+                                    </div>
+                                </div> <!-- /.blog-list-post -->
+                                <div class="blog-list-post clearfix">
+                                    <div class="blog-list-thumb">
+                                        <a href="blog-single.html"><img src="images/blog/blog-small-thumb2.jpg" alt=""></a>
+                                    </div>
+                                    <div class="blog-list-details">
+                                        <h5 class="blog-list-title"><a href="blog-single.html">Visiting Artists: Giles Bailey</a></h5>
+                                        <p class="blog-list-meta small-text"><span><a href="index.html#">12 January 2014</a></span> with <span><a href="index.html#">3 comments</a></span></p>
+                                    </div>
+                                </div> <!-- /.blog-list-post -->
+                                <div class="blog-list-post clearfix">
+                                    <div class="blog-list-thumb">
+                                        <a href="blog-single.html"><img src="images/blog/blog-small-thumb3.jpg" alt=""></a>
+                                    </div>
+                                    <div class="blog-list-details">
+                                        <h5 class="blog-list-title"><a href="blog-single.html">Workshop: Theories of the Image</a></h5>
+                                        <p class="blog-list-meta small-text"><span><a href="index.html#">12 January 2014</a></span> with <span><a href="index.html#">3 comments</a></span></p>
+                                    </div>
+                                </div> <!-- /.blog-list-post -->
+                            </div> <!-- /.widget-inner -->
+                        </div> <!-- /.widget-main -->
+                    </div> <!-- /col-md-6 -->
+                    
+                    <!-- Show Latest Events List -->
+                    <div class="col-md-6">
+                        <div class="widget-main">
+                            <div class="widget-main-title">
+                                <h4 class="widget-title">Events</h4>
+                            </div> <!-- /.widget-main-title -->
+                            <div class="widget-inner">
+                                <div class="event-small-list clearfix">
+                                    <div class="calendar-small">
+                                        <span class="s-month">Jan</span>
+                                        <span class="s-date">24</span>
+                                    </div>
+                                    <div class="event-small-details">
+                                        <h5 class="event-small-title"><a href="event-single.html">Nelson Mandela Memorial Tribute</a></h5>
+                                        <p class="event-small-meta small-text">Cramton Auditorium 9:00 AM to 1:00 PM</p>
+                                    </div>
+                                </div>
+                                <div class="event-small-list clearfix">
+                                    <div class="calendar-small">
+                                        <span class="s-month">Jan</span>
+                                        <span class="s-date">24</span>
+                                    </div>
+                                    <div class="event-small-details">
+                                        <h5 class="event-small-title"><a href="event-single.html">OVADA Oxford Open</a></h5>
+                                        <p class="event-small-meta small-text">Posner Center 4:30 PM to 6:00 PM</p>
+                                    </div>
+                                </div>
+                                <div class="event-small-list clearfix">
+                                    <div class="calendar-small">
+                                        <span class="s-month">Jan</span>
+                                        <span class="s-date">24</span>
+                                    </div>
+                                    <div class="event-small-details">
+                                        <h5 class="event-small-title"><a href="event-single.html">Filming Objects And Sculpture</a></h5>
+                                        <p class="event-small-meta small-text">A70 Cyert Hall 12:00 PM to 1:00 PM</p>
+                                    </div>
+                                </div>
+                            </div> <!-- /.widget-inner -->
+                        </div> <!-- /.widget-main -->
+                    </div> <!-- /.col-md-6 -->
+                    
+                </div> <!-- /.row -->
+                
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="widget-main">
+                            <div class="widget-main-title">
+                                <h4 class="widget-title">Our Campus</h4>
+                            </div> <!-- /.widget-main-title -->
+                            <div class="widget-inner">
+                                <div class="our-campus clearfix">
+                                    <ul>
+                                        <li><img src="images/campus/campus-logo1.jpg" alt=""></li>
+                                        <li><img src="images/campus/campus-logo2.jpg" alt=""></li>
+                                        <li><img src="images/campus/campus-logo3.jpg" alt=""></li>
+                                        <li><img src="images/campus/campus-logo4.jpg" alt=""></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div> <!-- /.widget-main -->
+                    </div> <!-- /.col-md-12 -->
+                </div> <!-- /.row -->
+
+            </div> <!-- /.col-md-8 -->
+            
+            <!-- Here begin Sidebar -->
+            <div class="col-md-4">
+
+                <div class="widget-main">
+                    <div class="widget-main-title">
+                        <h4 class="widget-title">Top Professors</h4>
+                    </div>
+                    <div class="widget-inner">
+                        <div class="prof-list-item clearfix">
+                           <div class="prof-thumb">
+                                <img src="images/prof/prof1.jpg" alt="Profesor Name">
+                            </div> <!-- /.prof-thumb -->
+                            <div class="prof-details">
+                                <h5 class="prof-name-list">Prof. Betty Hunt</h5>
+                                <p class="small-text">Sed ut lectus ac lacus molestie posuere non tincidunt arcu.</p>
+                            </div> <!-- /.prof-details -->
+                        </div> <!-- /.prof-list-item -->
+                        <div class="prof-list-item clearfix">
+                           <div class="prof-thumb">
+                                <img src="images/prof/prof2.jpg" alt="Profesor Name">
+                            </div> <!-- /.prof-thumb -->
+                            <div class="prof-details">
+                                <h5 class="prof-name-list">Prof. Victor Johns</h5>
+                                <p class="small-text">Nullam sollicitudin libero ut ullamcorper pretium.</p>
+                            </div> <!-- /.prof-details -->
+                        </div> <!-- /.prof-list-item -->
+                        <div class="prof-list-item clearfix">
+                           <div class="prof-thumb">
+                                <img src="images/prof/prof3.jpg" alt="Profesor Name">
+                            </div> <!-- /.prof-thumb -->
+                            <div class="prof-details">
+                                <h5 class="prof-name-list">Prof. Charles Conway</h5>
+                                <p class="small-text">Integer et nisl tincidunt, euismod orci eget, posuere nunc.</p>
+                            </div> <!-- /.prof-details -->
+                        </div> <!-- /.prof-list-item -->
+                    </div> <!-- /.widget-inner -->
+                </div> <!-- /.widget-main -->
+
+                <div class="widget-main">
+                    <div class="widget-main-title">
+                        <h4 class="widget-title">Testimonial</h4>
+                    </div>
+                    <div class="widget-inner">
+                        <div id="slider-testimonials">
+                            <ul>
+                                <li>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, quos, veniam optio voluptas hic delectus soluta odit nemo harum <strong class="dark-text">Shannon D. Edwards</strong></p>
+                                </li>
+                                <li>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, quos, veniam optio voluptas hic delectus soluta odit nemo harum <strong class="dark-text">Shannon D. Edwards</strong></p>
+                                </li>
+                                <li>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, quos, veniam optio voluptas hic delectus soluta odit nemo harum <strong class="dark-text">Shannon D. Edwards</strong></p>
+                                </li>
+                            </ul>
+                            <a class="prev fa fa-angle-left" href="index.html"></a>
+                            <a class="next fa fa-angle-right" href="index.html"></a>
+                        </div>
+                    </div> <!-- /.widget-inner -->
+                </div> <!-- /.widget-main -->
+
+                <div class="widget-main">
+                    <div class="widget-main-title">
+                        <h4 class="widget-title">Our Gallery</h4>
+                    </div>
+                    <div class="widget-inner">
+                        <div class="gallery-small-thumbs clearfix">
+                            <div class="thumb-small-gallery">
+                                <a class="fancybox" rel="gallery1" href="images/gallery/gallery1.jpg" title="Gallery Tittle One">
+                                    <img src="images/gallery/gallery-small-thumb1.jpg" alt="" />
+                                </a>
+                            </div>
+                            <div class="thumb-small-gallery">
+                                <a class="fancybox" rel="gallery1" href="images/gallery/gallery2.jpg" title="Gallery Tittle One">
+                                    <img src="images/gallery/gallery-small-thumb2.jpg" alt="" />
+                                </a>
+                            </div>
+                            <div class="thumb-small-gallery">
+                                <a class="fancybox" rel="gallery1" href="images/gallery/gallery3.jpg" title="Gallery Tittle One">
+                                    <img src="images/gallery/gallery-small-thumb3.jpg" alt="" />
+                                </a>
+                            </div>
+                            <div class="thumb-small-gallery">
+                                <a class="fancybox" rel="gallery1" href="images/gallery/gallery4.jpg" title="Gallery Tittle One">
+                                    <img src="images/gallery/gallery-small-thumb4.jpg" alt="" />
+                                </a>
+                            </div>
+                            <div class="thumb-small-gallery">
+                                <a class="fancybox" rel="gallery1" href="images/gallery/gallery5.jpg" title="Gallery Tittle One">
+                                    <img src="images/gallery/gallery-small-thumb5.jpg" alt="" />
+                                </a>
+                            </div>
+                            <div class="thumb-small-gallery">
+                                <a class="fancybox" rel="gallery1" href="images/gallery/gallery6.jpg" title="Gallery Tittle One">
+                                    <img src="images/gallery/gallery-small-thumb6.jpg" alt="" />
+                                </a>
+                            </div>
+                            <div class="thumb-small-gallery">
+                                <a class="fancybox" rel="gallery1" href="images/gallery/gallery7.jpg" title="Gallery Tittle One">
+                                    <img src="images/gallery/gallery-small-thumb7.jpg" alt="" />
+                                </a>
+                            </div>
+                            <div class="thumb-small-gallery">
+                                <a class="fancybox" rel="gallery1" href="images/gallery/gallery8.jpg" title="Gallery Tittle One">
+                                    <img src="images/gallery/gallery-small-thumb8.jpg" alt="" />
+                                </a>
+                            </div>
+                        </div> <!-- /.galler-small-thumbs -->
+                    </div> <!-- /.widget-inner -->
+                </div> <!-- /.widget-main -->
+
+            </div>
+        </div>
+    </div>

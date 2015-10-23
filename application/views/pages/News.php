705908a7dc5a03b435e8b500cd6ae8bab7e511c6
NAHID <NAHID@NAHID-PC>
2015-10-23 22:18:14 +0600

    commit by nahid

---
+++ application/views/pages/News.php
@@ -0,0 +1,132 @@
+
+    
+    <!-- Being Page Title -->
+    <div class="container">
+        <div class="page-title clearfix">
+            <div class="row">
+                <div class="col-md-12">
+                    <h6><a href="index.html">Home</a></h6>
+                    <h6><span class="page-active">News</span></h6>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+
+            <!-- Here begin Main Content -->
+            <div class="col-md-8">
+                <div class="row">
+
+                    <div class="col-md-6 col-sm-6">
+                        <div class="blog-grid-item">
+                            <div class="blog-grid-thumb">
+                                <a href="blog.html#" class="cat-blog">Business</a>
+                                <a href="blog-single.html">
+                                    <img src="images/blog/blog-thumb1.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="box-content-inner">
+                                <h5 class="blog-grid-title"><a href="blog-single.html">When a Doctor’s Visit Is a Guilt Trip</a></h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, cum, quidem aut natus odit deleniti placeat quia est quibusdam hic. Quod, minima, excepturi eum repellat tempora... <a href="event-single.html">View Details &rarr;</a></p>
+                                </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.blog-grid-item -->
+                    </div> <!-- /.col-md-6 -->
+
+                    <div class="col-md-6 col-sm-6">
+                        <div class="blog-grid-item">
+                            <div class="blog-grid-thumb">
+                                <a href="blog.html#" class="cat-blog">Government</a>
+                                <a href="blog-single.html">
+                                    <img src="images/blog/blog-thumb2.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="box-content-inner">
+                                <h5 class="blog-grid-title"><a href="blog-single.html">Unlocking the scrolls of Herculaneum</a></h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, cum, quidem aut natus odit deleniti placeat quia est quibusdam hic. Quod, minima, excepturi eum repellat tempora... <a href="event-single.html">View Details &rarr;</a></p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.blog-grid-item -->
+                    </div> <!-- /.col-md-6 -->
+
+                    <div class="col-md-6 col-sm-6">
+                        <div class="blog-grid-item">
+                            <div class="blog-grid-thumb">
+                                <a href="blog.html#" class="cat-blog">Tech & Engineering</a>
+                                <a href="blog-single.html">
+                                    <img src="images/blog/blog-thumb3.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="box-content-inner">
+                                <h5 class="blog-grid-title"><a href="blog-single.html">Corin Sworn wins Max Mara Art Prize</a></h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, cum, quidem aut natus odit deleniti placeat quia est quibusdam hic. Quod, minima, excepturi eum repellat tempora... <a href="event-single.html">View Details &rarr;</a></p></div> <!-- /.box-content-inner -->
+                        </div> <!-- /.blog-grid-item -->
+                    </div> <!-- /.col-md-6 -->
+
+                    <div class="col-md-6 col-sm-6">
+                        <div class="blog-grid-item">
+                            <div class="blog-grid-thumb">
+                                <a href="blog.html#" class="cat-blog">Family</a>
+                                <a href="blog-single.html">
+                                    <img src="images/blog/blog-thumb4.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="box-content-inner">
+                                <h5 class="blog-grid-title"><a href="blog-single.html">Graduate Open Day at the Ruskin</a></h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, cum, quidem aut natus odit deleniti placeat quia est quibusdam hic. Quod, minima, excepturi eum repellat tempora... <a href="event-single.html">View Details &rarr;</a></p></div> <!-- /.box-content-inner -->
+                        </div> <!-- /.blog-grid-item -->
+                    </div> <!-- /.col-md-6 -->
+
+                    <div class="col-md-6 col-sm-6">
+                        <div class="blog-grid-item">
+                            <div class="blog-grid-thumb">
+                                <a href="blog.html#" class="cat-blog">Science</a>
+                                <a href="blog-single.html">
+                                    <img src="images/blog/blog-thumb5.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="box-content-inner">
+                                <h5 class="blog-grid-title"><a href="blog-single.html">Visiting Artists: Giles Bailey</a></h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, cum, quidem aut natus odit deleniti placeat quia est quibusdam hic. Quod, minima, excepturi eum repellat tempora... <a href="event-single.html">View Details &rarr;</a></p></div> <!-- /.box-content-inner -->
+                        </div> <!-- /.blog-grid-item -->
+                    </div> <!-- /.col-md-6 -->
+
+                    <div class="col-md-6 col-sm-6">
+                        <div class="blog-grid-item">
+                            <div class="blog-grid-thumb">
+                                <a href="blog.html#" class="cat-blog">Student Achievements</a>
+                                <a href="blog-single.html">
+                                    <img src="images/blog/blog-thumb6.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="box-content-inner">
+                                <h5 class="blog-grid-title"><a href="blog-single.html">Workshop: Theories of the Image</a></h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, cum, quidem aut natus odit deleniti placeat quia est quibusdam hic. Quod, minima, excepturi eum repellat tempora... <a href="event-single.html">View Details &rarr;</a></p></div> <!-- /.box-content-inner -->
+                        </div> <!-- /.blog-grid-item -->
+                    </div> <!-- /.col-md-6 -->
+
+                </div> <!-- /.row -->
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="load-more-btn">
+                            <a href="blog.html#">Click here to load more News</a>
+                        </div>
+                    </div> <!-- /.col-md-12 -->
+                </div> <!-- /.row -->
+
+            </div> <!-- /.col-md-8 -->
+
+            <!-- Here begin Sidebar -->
+            <div class="col-md-4">
+
+                <?php 
+                    $this->load->view('templates/NoticeSidebar');
+                    $this->load->view('templates/NewsSidebar');
+                    $this->load->view('templates/EventsSidebar');
+                ?>
+
+            </div> <!-- /.col-md-4 -->
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->

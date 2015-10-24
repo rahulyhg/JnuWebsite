5cd3aa877b7db4ebb780915be2461077864f8f8a
NAHID <NAHID@NAHID-PC>
2015-10-24 20:31:44 +0600

    Merge origin/master
    Conflicts:
    	application/views/pages/News.php

--- application/views/Academics/faculty.php
+++ application/views/Academics/faculty.php
@@ -1,1 +1,78 @@
-<DIV class="container">Hello world</DIV>
+
+<!-- Being Page Title -->
+<div class="container">
+    <div class="page-title clearfix">
+        <div class="row">
+            <div class="col-md-12">
+                <h6><a href="index.html">Home</a></h6>
+                <h6><span class="page-active">Academics</span></h6>
+                <h6><span class="page-active">Faculty of Arts</span></h6>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-4">                
+            <div class="widget-main">
+                <div class="widget-main-title">
+                    <a href="<?php echo base_url(); ?>academics/index"><h4 class="widget-title">Faculty of Arts</h4></a>
+                </div>
+                <div class="widget-inner">
+                    <ul class="mixitup-controls">
+                        <li><a href="http://www.du.ac.bd/academic/department_item/ARB">Department of Arabic</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/BNG">Department of Bengali</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/CRL">Department of World Religions and Culture</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/DMUSIC">Department of Music</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/ENG">Department of English</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/HIS">Department of History</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/IHC">Department of Islamic History &amp; Culture</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/IST">Department of Islamic Studies</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/LIN">Department of Linguistics</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/LIS">Department of  Information Science and Library Management</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/PERS">Department of Persian Language and Literature</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/PHI">Department of Philosophy</a></li>
+                        <li><a href="http://www.du.ac.bd/academic/department_item/SPL">Department of Sanskrit</a></li>
+                    </ul>
+                </div> <!-- /.widget-inner -->
+            </div> <!-- /.widget-main -->
+
+
+
+        </div> <!-- /.col-md-3 -->
+
+        <div class="col-md-8">
+            <div class="row">
+
+                <div class="col-md-12">
+                        <div class="blog-post-container">
+                            
+                            <div class="blog-post-inner">
+                                <img src="<?php echo base_url(); ?>images/prof/prof2.jpg" width="200" height="200" alt="">
+                                <div class="event-contact">
+                                    <i><h4>Head of Office:  Dr. Md Akhtaruzzaman</h4>
+                                        <h4>Designation:  Dean</h4></i>
+                                </div>
+                                <br/>
+                                <h3 class="blog-post-title">Faculty of Arabic</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, quis, itaque culpa rem excepturi cum sunt quae obcaecati corrupti fugiat facere error eos necessitatibus iure ut voluptates dolor enim provident!</p>
+                                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, perspiciatis, ad, pariatur placeat facere laborum aspernatur quidem sint illum eos mollitia repellendus consectetur obcaecati possimus cupiditate delectus dolores eaque blanditiis?</strong></p>
+                                <p>
+                                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, cum, quos ipsam eligendi accusantium quo amet quae commodi obcaecati nobis ratione soluta. Voluptates, obcaecati consectetur officia saepe rerum animi doloremque!</blockquote>
+                                </p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, magni, blanditiis, excepturi accusantium fugiat enim accusamus non libero doloribus doloremque illo error perferendis sequi dignissimos nesciunt voluptates nulla voluptatibus optio.</p>
+                            </div>
+                        </div> <!-- /.blog-post-container -->
+                    </div> <!-- /.col-md-12 -->
+
+            </div> <!-- /.row -->
+
+
+        </div> <!-- /.col-md-9 -->
+
+    </div> <!-- /.row -->
+
+</div> <!-- /.container -->
---
+++ application/views/templates/TopMenu.php
@@ -0,0 +1,29 @@
+<li><a href="<?php echo base_url(); ?>">Home</a></li>
+<li><a href="javascript:void(0);">Events</a>
+    <ul class="sub-menu">
+        <li><a href="events-grid.html">Events Grid</a></li>
+        <li><a href="events-list.html">Events List</a></li>
+        <li><a href="event-single.html">Events Details</a>
+    </ul>
+</li>
+<li><a href="javascript:void(0);">Courses</a>
+    <ul class="sub-menu">
+        <li><a href="courses.html">Courses List</a></li>
+        <li><a href="course-single.html">Course Single</a></li>
+    </ul>
+</li>
+<li><a href="javascript:void(0);">Blog Entries</a>
+    <ul class="sub-menu">
+        <li><a href="blog.html">Blog Grid</a></li>
+        <li><a href="blog-single.html">Blog Single</a></li>
+        <li><a href="blog-disqus.html">Blog Disqus</a></li>
+    </ul>
+</li>
+<li><a href="javascript:void(0);">Pages</a>
+    <ul class="sub-menu">
+        <li><a href="archives.html">Archives</a></li>
+        <li><a href="shortcodes.html">Shortcodes</a></li>
+        <li><a href="gallery.html">Our Gallery</a></li>
+    </ul>
+</li>
+<li><a href="<?php echo base_url(); ?>contact">Contact</a></li>

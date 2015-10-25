f25d5ac302b71a29bce5d1ddb9998ee92e64f401
NAHID <NAHID@NAHID-PC>
2015-10-25 23:13:58 +0600

    Merge origin/master
    Conflicts:
    	application/views/pages/News.php

--- application/controllers/academics.php
+++ application/controllers/academics.php
@@ -2,15 +2,15 @@
 
 class Academics extends CI_Controller {
 
-	public function Index($page = 'faculty')
-	{
-            if(!file_exists('application/views/academics/'.$page.'.php')) {echo 'no file';return ;}
+    public function index(){
+        echo 'comming soon';
+    }
 
-            
-            $data['title']= ucfirst($page);
-            $this->load->view('templates/Header',$data);
-            $this->load->view('academics/'.$page);
-            $this->load->view('templates/Footer');
-	}
-        
+    public function faculty() {
+        $data['title'] = 'Faculty';
+        $this->load->view('templates/Header', $data);
+        $this->load->view('academics/faculty');
+        $this->load->view('templates/Footer');
+    }
+
 }

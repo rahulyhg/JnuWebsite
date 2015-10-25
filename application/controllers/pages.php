7fd353c10eb9d59b235a96959aac6919b03df7a7
NAHID <NAHID@NAHID-PC>
2015-10-25 22:54:16 +0600

    Merge origin/master
    Conflicts:
    	application/views/pages/News.php

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -9,18 +9,37 @@ class Pages extends CI_Controller {
             $this->load->view('pages/Home');
             $this->load->view('templates/Footer');
 	}
-        public function Contact(){
+        public function contact(){
             $data['title']='Contact';
             $this->load->view('templates/Header',$data);
             $this->load->view('pages/Contact');
             $this->load->view('templates/Footer');
         }
-        public function News()
+        public function news()
 	{            
             $data['title']= 'News';
             $this->load->view('templates/Header',$data);
             $this->load->view('pages/News');
             $this->load->view('templates/Footer');
 	}
-        
+        public function newsDetails()
+	{            
+            $data['title']= 'News';
+            $this->load->view('templates/Header',$data);
+            $this->load->view('pages/NewsDetails');
+            $this->load->view('templates/Footer');
+	}public function events()
+	{            
+            $data['title']= 'Events';
+            $this->load->view('templates/Header',$data);
+            $this->load->view('pages/Events');
+            $this->load->view('templates/Footer');
+	}
+        public function eventDetails()
+	{            
+            $data['title']= 'Event';
+            $this->load->view('templates/Header',$data);
+            $this->load->view('pages/EventDetails');
+            $this->load->view('templates/Footer');
+	}
 }
